e57ef076074caed8499a85a9cb5285b1c23212a3
ForLW <15704530687@163.com>
2021-11-27 11:54:32 +0800

    Add files via upload

--- clock_window.py
+++ clock_window.py
@@ -13,6 +13,7 @@ class Application(tkinter.Frame):
         self.master = master
         self.pack()
         self.label_title = tkinter.Label(self)
+        self.label_date = tkinter.Label(self)
         self.label_time = tkinter.Label(self)
         self.button1 = tkinter.Button(self)
         self.create_widgets()
@@ -23,17 +24,22 @@ class Application(tkinter.Frame):
         self.label_title.pack(pady=50)
         self.label_title.pack(expand=1)
 
-        self.label_time["text"] = time.time()
+        # self.label_date["text"] = ""
+        self.label_date["font"] = ("楷体", 20)
+        self.label_date.pack(pady=10)
+        # self.label_time["text"] = ""
         self.label_time["font"] = ("楷体", 20)
-        self.label_time.pack(pady=20)
+        self.label_time.pack(pady=10)
 
-        self.button1["text"] = "计时"
+        self.button1["text"] = "开始计时"
         self.button1["font"] = ("楷体", 20)
         self.button1["command"] = click_thing
         self.button1.pack(pady=50)
 
         while True:
-            self.label_time["text"] = time.time()
+            time_now = time.localtime(time.time())
+            self.label_date["text"] = "{}年{:0<2d}月{:0<2d}日".format(time_now.tm_year, time_now.tm_mon, time_now.tm_mday)
+            self.label_time["text"] = "{:0>2d}:{:0>2d}:{:0>2d}".format(time_now.tm_hour, time_now.tm_min, time_now.tm_sec)
             self.label_time.update()
             time.sleep(0.1)
 
